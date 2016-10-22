e0b63e82d22e2bd460b84e132aa637388d8c54f0
Sam Cate <SamCate@Sams-MacBook-Pro.local>
2016-10-22 09:07:03 -0400

    Connect Page routes and connected Angular

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,3 +1,19 @@
 app.controller('mainCtrl', function($scope) {
-  $scope.var = 'boat';
+  $scope.items = [
+    {
+      title: 'Dirt Bike',
+      price: 1200,
+      inventory: 3
+    },
+    {
+      title: 'ATV',
+      price: 2100,
+      inventory: 2
+    },
+    {
+      title: 'Moped',
+      price: 500,
+      inventory: 5
+    },
+  ]
 })
